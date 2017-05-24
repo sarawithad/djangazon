21effacb5d537a436622e0a1675ce8b5d1c13a3a
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-05-24 14:36:50 -0500

    test written but not passing

--- website/tests.py
+++ website/tests.py
@@ -40,18 +40,70 @@ class ProductDetailViewTest(TestCase):
             password = "abcd1234"
         )
 
-
+    #Verify that when a product is created that the Product Detail view has the correct product in the response context
     def test_product_detail_view_shows_correct_product(self):
         response = self.client.get(reverse('website:single_product', args=([self.product.pk])))
         self.assertEqual(response.context['product'].title, 'emoji stickers')
 
 
+    #Verify that when a product is created that the Product Detail view has the title, description, price and quantity are in the response body
     def test_product_detail_view_shows_product_details(self):
         response = self.client.get(reverse('website:single_product', args=([self.product.pk])))
         self.assertContains(response, "emoji stickers")
         self.assertContains(response, "yay!")
         self.assertContains(response, "1.99")
         self.assertContains(response, "500")
+
+
+
+
+class PaymentTypesViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username = "mducharme",
+            email = "meg@meg.com",
+            password = "abcd1234",
+            first_name = "Meg",
+            last_name = "Ducharme"
+        )
+
+        self.payment_type = PaymentType.objects.create(
+            payment_type_name = "Visa",
+            account_number = 1234123412341234,
+            customer = self.user
+        )
+
+        self.payment_type = PaymentType.objects.create(
+            payment_type_name = "MasterCard",
+            account_number = 5678567856785678,
+            customer = self.user
+        )
+
+        self.payment_type = PaymentType.objects.create(
+            payment_type_name = "AMEX",
+            account_number = 1010101010101010,
+            customer = self.user
+        )
+
+        self.client.login(
+            username = "mducharme",
+            password = "abcd1234"
+        )
+
+
+    #Verify that the Payment Types view for a customer has all of the payment types in the request context
+    def test_payment_type_view_shows_payment_types(self):
+        response = self.client.get(reverse('website:user_payment_types', args=([self.payment_type.pk])))
+        self.assertContains(response, "Visa")
+        self.assertContains(response, "MasterCard")
+        self.assertContains(response, "AMEX")
+
+
+
+
+
+
 
 
 
