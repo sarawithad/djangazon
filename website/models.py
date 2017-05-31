81ad429c53f19401090b008f93468c6ced43dbe7
Jordan Nelson <jnelsontn@gmail.com>
2017-05-31 10:30:16 -0500

    updated model, removed unnessary import

--- website/models.py
+++ website/models.py
@@ -38,7 +38,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, null=False)
     quantity = models.IntegerField()
     quantity_sold = models.IntegerField(default=0)
-    product_photo = models.ImageField() 
+    product_photo = models.ImageField(blank=True, null=True) 
 
     def __str__(self):
         return self.title
