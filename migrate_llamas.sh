758c8fde9d86ba8707ef36d7e878e5c587b2531b
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-06-29 09:59:09 -0500

    updated fixtures

--- migrate_llamas.sh
+++ migrate_llamas.sh
@@ -1,5 +1,4 @@
-find . -path "/djangazon/website/migrations/*.py" -not -name "__init__.py" -delete; #deletes all of the .py files in the migrations directory except for the __init__.py file.
-find . -path "/djangazon/website/migrations/*.pyc"  -delete; #deletes all of the .pyc files in the migrations directory.
+#!/bin/bash
 rm db.sqlite3; #deletes the database file.
 rm -rf website/migrations; #deletes the migrations folder.
 python manage.py migrate; #run the initial django migration to create all the initial tables. need this step because we are killing the database just above
